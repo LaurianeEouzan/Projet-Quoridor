a99030f58ab30f57fabeb0787d2848238649ee61
Lauriane Eouzan <laeou@ulaval.ca>
2025-10-30 18:55:52 -0400

    créer une partie api.py

--- api.py
+++ api.py
@@ -1,2 +1,17 @@
-print("happybirthday")
+import requests
 
+URL_BASE = "https://pax.ulaval.ca/quoridor/api/a25"
+
+def créer_une_partie(IDUL, secret):
+    try:
+        reponses = requests.post(f"{URL_BASE}/jeux", auth=(IDUL, secret))
+
+        if reponses.status_code == 200:
+            informations = reponses.json()
+            return informations['id'], informations
+        elif reponses.status_code == 401:
+            raise PermissionError(reponses.text)
+        elif reponses.status_code == 406:
+            raise RuntimeError(reponses.text)
+        else:
+            raise ConnectionError()
